57498f856233e2b2b366a264aaeb6b647ee4b87c
2171490100064 <2171490100064@idm.gad.nec.com.cn>
2016-12-12 15:40:31 +0800

    Modified the module for windows

--- getContent.el
+++ getContent.el
@@ -2,10 +2,10 @@
 "true" ; exec emacs --script "$0" "$@"
 (require 'ox-publish)
 
-(defun eob-get-content(context_str)
+(defun eob-get-content(orgFile)
   (interactive)
-  (with-temp-buffer
-    (insert context_str)
+   (with-temp-buffer
+    (insert-file-contents orgFile)
     (org-html-export-as-html nil nil nil t nil)
     (setq org-html (substring-no-properties (buffer-string)))
     (kill-buffer "*Org HTML Export*")
--- org_emacs_reader.py
+++ org_emacs_reader.py
@@ -1,16 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+import sys
+reload(sys)
+sys.setdefaultencoding("utf8")
 import re
 from pelican import signals
 from pelican.readers import BaseReader
 from pelican.utils import pelican_open
 import commands
-import os
-
+from bs4 import BeautifulSoup
 
 class OrgEmacsReader(BaseReader):
     enabled = True
     file_extensions = ['org']
 
+    def getstatusoutput(self, cmd):
+        """Return (status, output) of executing cmd in a shell."""
+        import sys
+        mswindows = (sys.platform == "win32")
+        import os
+        if not mswindows:
+            cmd = '{ ' + cmd + '; }'
+        pipe = os.popen(cmd + ' 2>&1', 'r')
+        text = pipe.read()
+        sts = pipe.close()
+        if sts is None: sts = 0
+        if text[-1:] == '\n': text = text[:-1]
+        return sts, text
+
     def read(self, filename):
+        print(filename)
         with pelican_open(filename) as fp:
             text = list(fp.splitlines())
 
@@ -22,11 +41,23 @@ class OrgEmacsReader(BaseReader):
                 value = meta_match.group(2).strip()
                 metadata[name] = self.process_metadata(name, value)
             else:
-                content = "\n".join(text[i:])
                 break
-        content="\"#+OPTIONS: toc:nil\n"+str(content)+"\""
-        status,content=commands.getstatusoutput("emacs -Q --script %s/getContent.el %s"%(os.path.split(os.path.realpath(__file__))[0],content))
-        return content, metadata
+        import sys
+        mswindows = (sys.platform == "win32")
+        import os
+        if not mswindows:
+            status,content=commands.getstatusoutput("emacs -Q --script %s/getContent.el %s"%(os.path.split(os.path.realpath(__file__))[0],filename))
+        else:
+            status,content=self.getstatusoutput("emacs -Q --script %s %s"%(os.path.join(os.path.split(os.path.realpath(__file__))[0],"getContent.el"),filename))
+        soup = BeautifulSoup(content, 'html.parser')
+        if soup.find(id="table-of-contents"):
+            soup.find(id="table-of-contents").extract()
+        content=soup.prettify()
+        h_level=range(2,6)
+        for n in reversed(h_level):
+            content=content.replace("<h"+str(n),"<h"+str(n+1))
+            content=content.replace("</h" + str(n), "</h" + str(n + 1))
+        return content,metadata
 
 def add_reader(readers):
     for ext in OrgEmacsReader.file_extensions:
